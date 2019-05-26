859dfed91425aba1ef0a87a0255d92acbcc30f66
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-05-26 08:44:41 -0500

    Solution for rust/binary-search

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,7 +1,26 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
-        key,
-        array
-    );
+use std::cmp::Ordering;
+
+pub fn find<T, U>(array: U, key: T) -> Option<usize>
+where
+    T: Ord,
+    U: AsRef<[T]>,
+{
+    let s = array.as_ref();
+    if s.is_empty() {
+        return None;
+    }
+    let mut size = s.len();
+    let mut base = 0_usize;
+    while size > 1 {
+        let half = size / 2;
+        let mid = base + half;
+        let cmp = s.get(mid).map(|elem| elem.cmp(&key)).unwrap();
+        base = if cmp == Ordering::Greater { base } else { mid };
+        size -= half;
+    }
+    if let Some(Ordering::Equal) = s.get(base).map(|elem| elem.cmp(&key)) {
+        Some(base)
+    } else {
+        None
+    }
 }
