ee2eaa1c99912299df1d5536ebaba5f4e4ff2a79
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-05-26 09:11:12 -0500

    Solution for rust/queen-attack

--- rust/queen-attack/src/lib.rs
+++ rust/queen-attack/src/lib.rs
@@ -1,31 +1,31 @@
 #[derive(Debug)]
-pub struct ChessPosition;
+pub struct ChessPosition {
+    rank: i32,
+    file: i32,
+}
 
 #[derive(Debug)]
-pub struct Queen;
+pub struct Queen {
+    position: ChessPosition,
+}
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        unimplemented!(
-            "Construct a ChessPosition struct, given the following rank, file: ({}, {}). If the position is invalid return None.",
-            rank,
-            file
-        );
+        match (rank, file) {
+            (0..=7, 0..=7) => Some(ChessPosition { rank, file }),
+            _ => None,
+        }
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        unimplemented!(
-            "Given the chess position {:?}, construct a Queen struct.",
-            position
-        );
+        Queen { position }
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-        unimplemented!(
-            "Determine if this Queen can attack the other Queen {:?}",
-            other
-        );
+        let (x1, y1) = (self.position.rank, self.position.file);
+        let (x2, y2) = (other.position.rank, other.position.file);
+        x1 == x2 || y1 == y2 || i32::abs(x1 - x2) == i32::abs(y1 - y2)
     }
 }
