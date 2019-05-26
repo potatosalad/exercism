1d7009c8968711e70669bce5d28843eb7e0a44ae
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-05-26 16:52:10 -0500

    Solution for rust/macros

--- rust/macros/src/lib.rs
+++ rust/macros/src/lib.rs
@@ -1,6 +1,16 @@
 #[macro_export]
 macro_rules! hashmap {
     () => {
-        unimplemented!()
+        ::std::collections::HashMap::new()
+    };
+    ($($key:expr => $value:expr,)+) => (hashmap!($($key => $value),+));
+    ($($key:expr => $value:expr),*) => {
+        {
+            let mut _hm = ::std::collections::HashMap::new();
+            $(
+                let _ = _hm.insert($key, $value);
+            )*
+            _hm
+        }
     };
 }
