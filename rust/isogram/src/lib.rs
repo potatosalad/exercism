6fb7a0f040c9e48bb8d4486089b16106f32d04c2
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-05-28 15:26:38 -0500

    Solution for rust/isogram

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -1,3 +1,15 @@
+use std::collections::HashSet;
+
 pub fn check(candidate: &str) -> bool {
-    unimplemented!("Is {} an isogram?", candidate);
+    let mut seen = HashSet::new();
+    for c in candidate.chars().flat_map(|c| c.to_lowercase()) {
+        if c.is_alphabetic() {
+            if seen.contains(&c) {
+                return false;
+            } else {
+                seen.insert(c);
+            }
+        }
+    }
+    true
 }
