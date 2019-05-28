2cff5006070a82d966cfb3617dc2f65f49cbdabb
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-05-27 20:33:07 -0500

    Solution for rust/palindrome-products

--- rust/palindrome-products/src/lib.rs
+++ rust/palindrome-products/src/lib.rs
@@ -1,22 +1,70 @@
+extern crate num_cpus;
+
+use std::collections::HashSet;
+use std::sync::mpsc;
+use std::thread;
+
 pub type Palindrome = u64;
+
 pub fn get_palindrome_products(min: u64, max: u64) -> Vec<Palindrome> {
-    unimplemented!(
-        "Find all palindromic numbers which are products of numbers in the inclusive range ({}..{})",
-        min,
-        max
-    )
+    let cpus = num_cpus::get();
+    if cpus == 1 {
+        let mut products: HashSet<Palindrome> = HashSet::new();
+        palindrome_products(min, max, 1, |product| {
+            products.insert(product);
+        });
+        products.into_iter().collect()
+    } else {
+        let mut pool = Vec::new();
+        let (tx, rx) = mpsc::channel::<Palindrome>();
+        for n in 0..cpus {
+            let tx = tx.clone();
+            pool.push(Some(thread::spawn(move || {
+                palindrome_products(min + n as u64, max, cpus, |product| {
+                    tx.send(product).unwrap();
+                });
+            })));
+        }
+        for handle in &mut pool {
+            if let Some(thread) = handle.take() {
+                thread.join().unwrap();
+            }
+        }
+        drop(tx);
+        rx.into_iter()
+            .collect::<HashSet<Palindrome>>()
+            .into_iter()
+            .collect()
+    }
+}
+
+fn palindrome_products<F>(min: u64, max: u64, step: usize, mut cb: F)
+where
+    F: FnMut(Palindrome),
+{
+    for i in (min..=max).step_by(step) {
+        for j in i..=max {
+            let product = i * j;
+            let digits = product.to_string();
+            if digits.chars().eq(digits.chars().rev()) {
+                cb(product);
+            }
+        }
+    }
 }
 
 pub fn min(palindromes: &[Palindrome]) -> Option<Palindrome> {
-    unimplemented!(
-        "Return the palindrome of minimal value from the supplied list: {:?}",
-        palindromes
-    )
+    palindromes.iter().fold(None, |min, &p| match min {
+        None => Some(p),
+        Some(q) if p < q => Some(p),
+        Some(q) => Some(q),
+    })
 }
 
 pub fn max(palindromes: &[Palindrome]) -> Option<Palindrome> {
-    unimplemented!(
-        "Return the palindrome of maximal value from the supplied list: {:?}",
-        palindromes
-    )
+    palindromes.iter().fold(None, |max, &p| match max {
+        None => Some(p),
+        Some(q) if p > q => Some(p),
+        Some(q) => Some(q),
+    })
 }
