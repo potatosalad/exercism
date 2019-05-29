7693835714123c7b4e3fe7f36096186b47902b71
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-05-29 16:40:22 -0500

    Solution for rust/allergies

--- rust/allergies/src/lib.rs
+++ rust/allergies/src/lib.rs
@@ -1,33 +1,34 @@
-pub struct Allergies;
+extern crate enum_iterator;
 
-#[derive(Debug, PartialEq)]
+use enum_iterator::IntoEnumIterator;
+
+pub struct Allergies(u32);
+
+#[derive(Clone, Copy, Debug, IntoEnumIterator, PartialEq)]
 pub enum Allergen {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats,
+    Eggs = 1,
+    Peanuts = 1 << 1,
+    Shellfish = 1 << 2,
+    Strawberries = 1 << 3,
+    Tomatoes = 1 << 4,
+    Chocolate = 1 << 5,
+    Pollen = 1 << 6,
+    Cats = 1 << 7,
 }
 
 impl Allergies {
     pub fn new(score: u32) -> Self {
-        unimplemented!(
-            "Given the '{}' score, construct a new Allergies struct.",
-            score
-        );
+        Allergies(score)
     }
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
-        unimplemented!(
-            "Determine if the patient is allergic to the '{:?}' allergen.",
-            allergen
-        );
+        let a = *allergen as u32;
+        (self.0 & a) == a
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
-        unimplemented!("Return the list of allergens contained within the score with which the Allergies struct was made.");
+        Allergen::into_enum_iter()
+            .filter(|allergen| self.is_allergic_to(&allergen))
+            .collect()
     }
 }
