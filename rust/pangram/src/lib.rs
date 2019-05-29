8d839c5992bf85b0d59f04ff8d730afee8e6e1c4
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-05-29 13:38:49 -0500

    Solution for rust/pangram

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -1,4 +1,27 @@
-/// Determine whether a sentence is a pangram.
+#[macro_use]
+extern crate lazy_static;
+
+use std::collections::HashSet;
+
 pub fn is_pangram(sentence: &str) -> bool {
-    unimplemented!("Is {} a pangram?", sentence);
+    let chars: HashSet<char> = sentence
+        .chars()
+        .filter(|c| c.is_alphabetic())
+        .flat_map(|c| c.to_lowercase())
+        .collect();
+    chars.eq(&ASCII_ALPHABET) || chars.eq(&GERMAN_ALPHABET) || chars.eq(&SPANISH_ALPHABET)
+}
+
+lazy_static! {
+    static ref ASCII_ALPHABET: HashSet<char> = (b'a'..=b'z').map(char::from).collect();
+    static ref GERMAN_ALPHABET: HashSet<char> = {
+        let mut alphabet = ASCII_ALPHABET.clone();
+        alphabet.extend(&['ä', 'ö', 'ü', 'ß']);
+        alphabet
+    };
+    static ref SPANISH_ALPHABET: HashSet<char> = {
+        let mut alphabet = ASCII_ALPHABET.clone();
+        alphabet.extend(&['ñ']);
+        alphabet
+    };
 }
