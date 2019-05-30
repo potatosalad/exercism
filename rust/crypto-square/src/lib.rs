3635b51eec4701d7a3b4b29f7c3638c9e54ad508
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-05-30 11:56:36 -0500

    Solution for rust/crypto-square

--- rust/crypto-square/src/lib.rs
+++ rust/crypto-square/src/lib.rs
@@ -1,3 +1,38 @@
 pub fn encrypt(input: &str) -> String {
-    unimplemented!("Encrypt {:?} using a square code", input)
+    let plaintext: Vec<char> = input
+        .chars()
+        .filter(|c| c.is_ascii_alphanumeric())
+        .map(|c| c.to_ascii_lowercase())
+        .collect();
+    if plaintext.is_empty() {
+        String::new()
+    } else {
+        let rows = (((plaintext.len() - 1) as f64).sqrt() as usize) + 1;
+        let cols = (0..rows)
+            .map(|r| {
+                (0..)
+                    .map(move |c| r + c * rows)
+                    .take_while(|&i| i < plaintext.len())
+                    .count()
+            })
+            .max()
+            .unwrap()
+            + 1;
+        let mut out: String = (0..rows)
+            .flat_map(|r| {
+                let mut row: Vec<char> = (0..)
+                    .map(move |c| r + c * rows)
+                    .take_while(|&i| i < plaintext.len())
+                    .map(|i| plaintext.get(i).unwrap())
+                    .cloned()
+                    .collect();
+                while row.len() < cols {
+                    row.push(' ');
+                }
+                row
+            })
+            .collect();
+        out.remove(out.len() - 1);
+        out
+    }
 }
