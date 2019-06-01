4739ffe08855d85bf23375ce310b17e04bc30b6a
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-06-01 13:06:40 -0500

    Solution for rust/atbash-cipher

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,9 +1,29 @@
-/// "Encipher" with the Atbash cipher.
+extern crate itertools;
+use itertools::Itertools;
+
 pub fn encode(plain: &str) -> String {
-    unimplemented!("Encoding of {:?} in Atbash cipher.", plain);
+    atbash_crypt(plain)
+        .chunks(5)
+        .into_iter()
+        .map(|chunk| chunk.collect::<String>())
+        .join(" ")
 }
 
-/// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    unimplemented!("Decoding of {:?} in Atbash cipher.", cipher);
+    atbash_crypt(cipher).collect()
+}
+
+fn atbash_crypt<'a>(
+    input: &'a str,
+) -> std::iter::FilterMap<std::str::Chars<'a>, fn(char) -> Option<char>> {
+    input.chars().filter_map(|c| match c {
+        '0'...'9' => Some(c),
+        'A'...'Z' => Some(atbash_rotate(c, b'A')),
+        'a'...'z' => Some(atbash_rotate(c, b'a')),
+        _ => None,
+    })
+}
+
+fn atbash_rotate(c: char, base: u8) -> char {
+    (b'a' + ((25 - (c as u8 - base)) % 26)) as char
 }
