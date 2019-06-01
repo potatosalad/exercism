f203e05afbfee57900fda0de11e13948bccc1aa4
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-06-01 13:51:48 -0500

    Solution for rust/rail-fence-cipher

--- rust/rail-fence-cipher/src/lib.rs
+++ rust/rail-fence-cipher/src/lib.rs
@@ -1,15 +1,57 @@
-pub struct RailFence;
+pub struct RailFence(usize);
 
 impl RailFence {
     pub fn new(rails: u32) -> RailFence {
-        unimplemented!("Construct a new fence with {} rails", rails)
+        assert!(rails != 0);
+        RailFence(rails as usize)
     }
 
     pub fn encode(&self, text: &str) -> String {
-        unimplemented!("Encode this text: {}", text)
+        if self.0 == 1 {
+            text.to_string()
+        } else {
+            self.positions(text)
+                .into_iter()
+                .map(|position| text.chars().nth(position).unwrap())
+                .collect()
+        }
     }
 
     pub fn decode(&self, cipher: &str) -> String {
-        unimplemented!("Decode this ciphertext: {}", cipher)
+        if self.0 == 1 {
+            cipher.to_string()
+        } else {
+            let positions = self.positions(cipher);
+            let mut text = vec![' '; positions.len()];
+            for (i, c) in cipher.chars().enumerate() {
+                let position = positions[i];
+                text[position] = c;
+            }
+            text.into_iter().collect()
+        }
+    }
+
+    fn positions(&self, input: &str) -> Vec<usize> {
+        assert!(self.0 > 1);
+        let length = input.chars().count();
+        let mut matrix = vec![vec![(' ', false); length]; self.0];
+        for (col, c) in input.chars().enumerate() {
+            let pad = 2 * self.0 - 2;
+            let row = if col % pad < pad / 2 {
+                col % pad
+            } else {
+                pad - col % pad
+            };
+            matrix[row][col] = (c, true);
+        }
+        let order: Vec<usize> = matrix
+            .into_iter()
+            .flat_map(|row| {
+                row.into_iter()
+                    .enumerate()
+                    .filter_map(|(col, (_, x))| if x { Some(col) } else { None })
+            })
+            .collect();
+        order
     }
 }
