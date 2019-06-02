36c89bb8d9344279dd3fec1b85fe924d2d526006
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-06-02 10:36:59 -0500

    Solution for rust/pig-latin

--- rust/pig-latin/src/lib.rs
+++ rust/pig-latin/src/lib.rs
@@ -1,6 +1,22 @@
 pub fn translate(input: &str) -> String {
-    unimplemented!(
-        "Using the Pig Latin text transformation rules, convert the given input '{}'",
-        input
-    );
+    input
+        .split(' ')
+        .map(anslatetray)
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+fn anslatetray(word: &str) -> String {
+    let mut i = if word.starts_with('y') {
+        word.find(|c| "aeiou".contains(c)).unwrap()
+    } else {
+        word.find(|c| "aeiouy".contains(c)).unwrap()
+    };
+    if i >= 1 && &word[i - 1..=i] == "qu" {
+        i += 1
+    }
+    if &word[..2] == "yt" || "ay" == &word[i..] {
+        i = 0
+    }
+    format!("{}{}ay", &word[i..], &word[..i])
 }
