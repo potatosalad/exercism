5917ad419d9854600717b4184b92c4b548883ec8
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-06-01 21:52:21 -0500

    Solution for rust/decimal

--- rust/decimal/src/lib.rs
+++ rust/decimal/src/lib.rs
@@ -1,10 +1,18 @@
-/// Type implementing arbitrary-precision decimal arithmetic
-pub struct Decimal {
-    // implement your type here
-}
+extern crate bigdecimal;
+#[macro_use]
+extern crate num_derive;
+extern crate num_traits;
+
+use bigdecimal::BigDecimal;
+use std::str::FromStr;
+#[derive(Debug, Eq, Num, NumOps, One, Ord, PartialEq, PartialOrd, Zero)]
+pub struct Decimal(BigDecimal);
 
 impl Decimal {
     pub fn try_from(input: &str) -> Option<Decimal> {
-        unimplemented!("Create a new decimal with a value of {}", input)
+        match BigDecimal::from_str(input) {
+            Ok(n) => Some(Decimal(n)),
+            _ => None,
+        }
     }
 }
