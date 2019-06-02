14c6a667f5a5230d4aa8cda87d00aeb4411d430e
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-06-02 10:51:38 -0500

    Solution for rust/minesweeper

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -1,3 +1,39 @@
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    unimplemented!("\nAnnotate each square of the given minefield with the number of mines that surround said square (blank if there are no surrounding mines):\n{:#?}\n", minefield);
+    minefield
+        .iter()
+        .enumerate()
+        .map(|(i, row)| {
+            row.chars()
+                .enumerate()
+                .map(|(j, c)| minecount(minefield, i, j, c))
+                .collect::<String>()
+        })
+        .collect()
+}
+
+fn minecount(minefield: &[&str], i: usize, j: usize, c: char) -> char {
+    if c != ' ' {
+        c
+    } else {
+        let mut count: u8 = 0;
+        for x in 0..=2 {
+            for y in 0..=2 {
+                if i + 1 < x
+                    || i + 1 - x >= minefield.len()
+                    || j + 1 < y
+                    || j + 1 - y >= minefield[i].len()
+                    || (x == 1 && y == 1)
+                {
+                    continue;
+                } else if &minefield[i + 1 - x][(j + 1 - y)..=(j + 1 - y)] == "*" {
+                    count += 1;
+                }
+            }
+        }
+        if count == 0 {
+            ' '
+        } else {
+            (count + b'0') as char
+        }
+    }
 }
