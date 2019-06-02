12e003b6aba4d1a4f2913504418c1d1ad6497e93
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-06-01 22:43:43 -0500

    Solution for rust/largest-series-product

--- rust/largest-series-product/src/lib.rs
+++ rust/largest-series-product/src/lib.rs
@@ -5,9 +5,26 @@ pub enum Error {
 }
 
 pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
-    unimplemented!(
-        "largest series product of a span of {} digits in {}",
-        span,
-        string_digits
-    );
+    if span > string_digits.len() {
+        Err(Error::SpanTooLong)
+    } else if span == 0 {
+        Ok(1)
+    } else {
+        let input: Vec<char> = string_digits.chars().collect();
+        let mut max_product: u64 = 0;
+        for window in input.windows(span) {
+            let mut product: u64 = 1;
+            for c in window {
+                if c.is_ascii_digit() {
+                    product *= (*c as u64) - u64::from(b'0');
+                } else {
+                    return Err(Error::InvalidDigit(*c));
+                }
+            }
+            if product > max_product {
+                max_product = product;
+            }
+        }
+        Ok(max_product)
+    }
 }
