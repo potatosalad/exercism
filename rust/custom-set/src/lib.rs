22a84d8229132e0d480e22d7d1fb56a1a6ad179a
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-06-02 10:27:09 -0500

    Solution for rust/custom-set

--- rust/custom-set/src/lib.rs
+++ rust/custom-set/src/lib.rs
@@ -1,61 +1,81 @@
 use std::fmt::Debug;
-use std::marker::PhantomData;
 
-#[derive(Debug, PartialEq)]
-pub struct CustomSet<T> {
-    // This field is here to make the template compile and not to
-    // complain about unused type parameter 'T'. Once you start
-    // solving the exercise, delete this field and the 'std::marker::PhantomData'
-    // import.
-    phantom: PhantomData<T>,
-}
+#[derive(Debug)]
+pub struct CustomSet<T: Clone + Debug + PartialEq>(Vec<T>);
 
-impl<T: Debug> CustomSet<T> {
+impl<T: Clone + Debug + PartialEq> CustomSet<T> {
     pub fn new(input: &[T]) -> Self {
-        unimplemented!(
-            "From the given input '{:?}' construct a new CustomSet struct.",
-            input
-        );
+        let mut set = Self::with_capacity(input.len());
+        for element in input {
+            set.add(element.clone());
+        }
+        set
+    }
+
+    pub fn empty() -> Self {
+        CustomSet(Vec::new())
+    }
+
+    pub fn with_capacity(capacity: usize) -> Self {
+        CustomSet(Vec::with_capacity(capacity))
     }
 
     pub fn contains(&self, element: &T) -> bool {
-        unimplemented!(
-            "Determine if the '{:?}' element is present in the CustomSet struct.",
-            element
-        );
+        self.0.contains(element)
     }
 
     pub fn add(&mut self, element: T) {
-        unimplemented!("Add the '{:?}' element to the CustomSet struct.", element);
+        if !self.contains(&element) {
+            self.0.push(element);
+        }
     }
 
     pub fn is_subset(&self, other: &Self) -> bool {
-        unimplemented!(
-            "Determine if the CustomSet struct is a subset of the other '{:?}' struct.",
-            other
-        );
+        self.0.iter().all(|element| other.contains(element))
     }
 
     pub fn is_empty(&self) -> bool {
-        unimplemented!("Determine if the CustomSet struct is empty.");
+        self.0.is_empty()
     }
 
     pub fn is_disjoint(&self, other: &Self) -> bool {
-        unimplemented!(
-            "Determine if the CustomSet struct and the other struct '{:?}' are disjoint.",
-            other
-        );
+        !self.0.iter().any(|element| other.contains(element))
     }
 
     pub fn intersection(&self, other: &Self) -> Self {
-        unimplemented!("Construct a new CustomSet struct that is an intersection between current struct and the other struct '{:?}'.", other);
+        let mut set = Self::empty();
+        for element in self.0.iter() {
+            if other.contains(element) {
+                set.add(element.clone());
+            }
+        }
+        set
     }
 
     pub fn difference(&self, other: &Self) -> Self {
-        unimplemented!("Construct a new CustomSet struct that is a difference between current struct and the other struct '{:?}'.", other);
+        let mut set = Self::empty();
+        for element in self.0.iter() {
+            if !other.contains(element) {
+                set.add(element.clone());
+            }
+        }
+        set
     }
 
     pub fn union(&self, other: &Self) -> Self {
-        unimplemented!("Construct a new CustomSet struct that is an union between current struct and the other struct '{:?}'.", other);
+        let mut set = Self::empty();
+        for element in self.0.iter().chain(other.0.iter()) {
+            set.add(element.clone());
+        }
+        set
+    }
+}
+
+impl<T: Clone + Debug + PartialEq> PartialEq for CustomSet<T>
+where
+    T: PartialEq,
+{
+    fn eq(&self, other: &CustomSet<T>) -> bool {
+        self.is_subset(other) && other.is_subset(self)
     }
 }
