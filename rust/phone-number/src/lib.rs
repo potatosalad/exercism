6b856894ccd4b0b64ad6189034b2ee12acf3a969
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-06-03 15:31:27 -0500

    Solution for rust/phone-number

--- rust/phone-number/src/lib.rs
+++ rust/phone-number/src/lib.rs
@@ -1,6 +1,18 @@
 pub fn number(user_number: &str) -> Option<String> {
-    unimplemented!(
-        "Given the number entered by user '{}', convert it into SMS-friendly format. If the entered number is not a valid NANP number, return None.",
-        user_number
-    );
+    let mut v: Vec<char> = user_number.chars().filter(|c| c.is_ascii_digit()).collect();
+    if v.len() == 11 {
+        if v[0] != '1' {
+            return None;
+        }
+        v.remove(0);
+    }
+    if v.len() == 10 {
+        if v[0] == '0' || v[0] == '1' || v[3] == '0' || v[3] == '1' {
+            None
+        } else {
+            Some(v.into_iter().collect())
+        }
+    } else {
+        None
+    }
 }
