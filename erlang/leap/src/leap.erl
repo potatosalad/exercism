945ba993f9be957ea302ec3de48fca8b66a4c29c
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-06-14 08:18:08 -0500

    Solution for erlang/leap

--- erlang/leap/src/leap.erl
+++ erlang/leap/src/leap.erl
@@ -2,5 +2,7 @@
 
 -export([leap_year/1]).
 
-
-leap_year(_Year) -> undefined.
+leap_year(Year) when Year rem 4 =:= 0 andalso (Year rem 100 =/= 0 orelse Year rem 400 =:= 0) ->
+    true;
+leap_year(_Year) ->
+    false.
