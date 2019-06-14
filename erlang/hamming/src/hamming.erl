22153becfb3c8c944634401332baa415718120d5
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-06-14 09:21:00 -0500

    Solutionf or erlang/hamming

--- erlang/hamming/src/hamming.erl
+++ erlang/hamming/src/hamming.erl
@@ -2,5 +2,15 @@
 
 -export([distance/2]).
 
+distance(Strand1, Strand2) when length(Strand1) =:= length(Strand2) ->
+    distance(Strand1, Strand2, 0);
+distance(_, _) ->
+    {error, "left and right strands must be of equal length"}.
 
-distance(_Strand1, _Strand2) -> undefined.
+%% @private
+distance([], [], C) ->
+    C;
+distance([H | T1], [H | T2], C) ->
+    distance(T1, T2, C);
+distance([_ | T1], [_ | T2], C) ->
+    distance(T1, T2, C + 1).
