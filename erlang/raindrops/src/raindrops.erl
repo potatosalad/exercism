25800d2b6e0d12937be1486a2c03a8cc3d7cbe05
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-06-14 09:05:41 -0500

    Solution for erlang/raindrops

--- erlang/raindrops/src/raindrops.erl
+++ erlang/raindrops/src/raindrops.erl
@@ -2,5 +2,23 @@
 
 -export([convert/1]).
 
+-spec convert(pos_integer()) -> string().
+convert(N) when is_integer(N) andalso N > 0 ->
+    case lists:flatten([pling(N), plang(N), plong(N)]) of
+        [] ->
+            erlang:integer_to_list(N);
+        R = [_ | _] ->
+            R
+    end.
 
-convert(_Number) -> undefined.
+%% @private
+pling(N) when N rem 3 =:= 0 -> "Pling";
+pling(_) -> [].
+
+%% @private
+plang(N) when N rem 5 =:= 0 -> "Plang";
+plang(_) -> [].
+
+%% @private
+plong(N) when N rem 7 =:= 0 -> "Plong";
+plong(_) -> [].
