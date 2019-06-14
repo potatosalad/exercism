c74b9e734a71c9de5ffc35edcaa1d10ca1b5c966
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-06-14 08:34:10 -0500

    Solution for erlang/collatz-conjecture

--- erlang/collatz-conjecture/src/collatz_conjecture.erl
+++ erlang/collatz-conjecture/src/collatz_conjecture.erl
@@ -2,5 +2,17 @@
 
 -export([steps/1]).
 
+-spec steps(pos_integer()) -> non_neg_integer();
+           (any()) -> {error, string()}.
+steps(N) when is_integer(N) andalso N > 0 ->
+    collatz(N, 0);
+steps(_) ->
+    {error, "Only positive numbers are allowed"}.
 
-steps(_N) -> undefined.
+%% @private
+collatz(1, Steps) ->
+    Steps;
+collatz(N, Steps) when N rem 2 =:= 0 ->
+    collatz(N div 2, Steps + 1);
+collatz(N, Steps) ->
+    collatz(3 * N + 1, Steps + 1).
