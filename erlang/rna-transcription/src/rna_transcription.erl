663439dcfed589e35d701318debffc062218b9f3
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-06-14 09:09:46 -0500

    Solution for erlang/rna-transcription

--- erlang/rna-transcription/src/rna_transcription.erl
+++ erlang/rna-transcription/src/rna_transcription.erl
@@ -2,5 +2,9 @@
 
 -export([to_rna/1]).
 
-
-to_rna(_Strand) -> undefined.
+-spec to_rna(string()) -> string().
+to_rna([]) -> [];
+to_rna([$G | Rest]) -> [$C | to_rna(Rest)];
+to_rna([$C | Rest]) -> [$G | to_rna(Rest)];
+to_rna([$T | Rest]) -> [$A | to_rna(Rest)];
+to_rna([$A | Rest]) -> [$U | to_rna(Rest)].
