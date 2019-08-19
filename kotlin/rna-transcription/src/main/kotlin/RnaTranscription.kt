5832c5de64219a879749341c17b131b087588b7e
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2019-08-18 19:16:04 -0500

    Solution for kotlin/rna-transcription

--- kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
+++ kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -1,1 +1,10 @@
-fun transcribeToRna(dna: String): String = ""
+fun transcribeToRna(dna: String): String =
+        dna.fold(String()) { acc, nucleotide ->
+            acc + when (nucleotide) {
+                'A' -> 'U'
+                'C' -> 'G'
+                'G' -> 'C'
+                'T' -> 'A'
+                else -> throw IllegalArgumentException("DNA may only contain nucleotides in A, C, G and T")
+            }
+        }
